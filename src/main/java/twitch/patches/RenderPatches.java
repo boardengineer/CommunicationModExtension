d5de305bfabd1bf2f1d11e7a8a0828643b91f199
boardengineer <77598527+boardengineer@users.noreply.github.com>
2022-02-16 01:27:44 -0800

    Patch out rendering the character select arrows

--- src/main/java/twitch/patches/RenderPatches.java
+++ src/main/java/twitch/patches/RenderPatches.java
@@ -1,4 +1,8 @@
+package twitch.patches;
+
+import basemod.CustomCharacterSelectScreen;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
@@ -19,6 +23,14 @@ public class RenderPatches {
     public static class NoRenderAscension {
         @SpirePrefixPatch
         public static SpireReturn doNothingAtRender(CharacterSelectScreen option, SpriteBatch sb) {
+            return SpireReturn.Return(null);
+        }
+    }
+
+    @SpirePatch(clz = CustomCharacterSelectScreen.class, method = "render")
+    public static class NoRenderSelectdoScreen {
+        @SpireInsertPatch(loc = 69)
+        public static SpireReturn noRenderArrows(CustomCharacterSelectScreen scree) {
             return SpireReturn.Return(null);
         }
     }
